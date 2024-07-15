1ecf532c6ee4c74c6de08c3c03c5fd8466627d55
Sakib-RUET-CSE17 <hsakib8685@gmail.com>
2024-07-15 12:33:17 +0600

    m6 task1 add

---
+++ module6/task1_validPalindrome.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi acos(-1.0)
+#define mod 1000000007
+
+bool checkPalindrome(string &s, int i, int j) {
+  if (i >= j) {
+    return true;
+  }
+  if (s[i] != s[j]) {
+    return false;
+  }
+  return checkPalindrome(s, i + 1, j - 1);
+}
+
+int main() {
+  string s = "madama";
+  cout << checkPalindrome(s, 0, s.size() - 1) << endl;
+  return 0;
+}
+
+/*
+Complexity:
+Time: O(n), where n is the size of the string
+Space: O(1)
+*/
